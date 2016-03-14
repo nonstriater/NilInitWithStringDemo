bb45172afac8e8c4b3004d329a83a69d1c5a0a61
冉文杰 <ranwenjie@didichuxing.com>
2016-03-14 16:29:43 +0800

    Initial Commit

---
+++ NilInitWithStringDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NilInitWithStringDemo
+//
+//  Created by ranwenjie on 16/3/14.
+//  Copyright © 2016年 ranwenjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NilInitWithStringDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NilInitWithStringDemo
+//
+//  Created by ranwenjie on 16/3/14.
+//  Copyright © 2016年 ranwenjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
